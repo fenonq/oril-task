a507b779a0cd4c25d66a922fd086791cc9c74892
fenonq <fenyk.ttt@gmail.com>
2022-12-22 17:37:14 +0200

    Initial commit

---
+++ src/main/java/com/fenonq/oriltask/OrilTaskApplication.java
@@ -0,0 +1,13 @@
+package com.fenonq.oriltask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrilTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrilTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fenonq/oriltask/OrilTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fenonq.oriltask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrilTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
